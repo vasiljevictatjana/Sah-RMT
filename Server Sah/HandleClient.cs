1745a1492efd53e48723d9d319b88ba27b51448a
Stefan Braković <brakovic.stefan@gmail.com>
2016-03-12 00:22:10 -0800

    First commit Alpha server and Stardet client handling

---
+++ Server Sah/HandleClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server_Sah
+{
+    class HandleClient
+    {
+        TcpClient clientSocket;
+        string clNo;
+        public void startClient(TcpClient inClientSocket, string clientNo)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clientNo;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = nul
+        }
+    }
+}
--- Server Sah/Program.cs
+++ Server Sah/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
 
 namespace Server_Sah
 {
@@ -10,6 +12,44 @@ namespace Server_Sah
     {
         static void Main(string[] args)
         {
+            //Creates a listener which is waiting for clients to ask to connect
+            TcpListener serverSocket = new TcpListener(30121);
+
+            //Creates a socket for client to grab to
+            TcpClient clientSocket = default(TcpClient);
+
+            //List of all clients
+            LinkedList<TcpClient> listOfClients = new LinkedList<TcpClient>();
+
+
+            // TODO maybe this need to be a static variable due to further ussage in to all chat function as an counter of connected clients
+            int counter;
+
+            //Starts the socket and now is ready to accept clients
+            serverSocket.Start();
+            Console.WriteLine(" >> " + "Server Running");
+
+            counter = 0;
+            while (true)
+            {
+                counter++;
+                clientSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine(" >> " + "Client No: " + Convert.ToSingle(counter) + " connected!");
+                listOfClients.AddFirst(clientSocket);
+
+                HandleClient client = new HandleClient();
+                client.startClient(clientSocket, Convert.ToString(counter));
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> " + "Server shutting down...");
+            Console.ReadLine();
+
+
         }
+
+
+
     }
 }
